b19b335c13f7c81febc291a4576410e9435ebccf
Finsa Nurpandi <finsaworks@gmail.com>
2022-11-03 15:53:45 +0700

    authentication and authorization

--- blog/app/Providers/AuthServiceProvider.php
+++ blog/app/Providers/AuthServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
+use App\Models\User;
+
 class AuthServiceProvider extends ServiceProvider
 {
     /**
@@ -23,6 +25,14 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Gate::define('manage-lecture', function(User $user){
+            return $user->role_id == 1;
+        });
+
+        Gate::define('manage-student', function(User $user){
+            return $user->role_id == 2;
+        });
+         
         $this->registerPolicies();
 
         //
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +10,25 @@ Route::get('/', function () {
 
 Route::get('/test', [TestController::class, 'index'])->name('test');
 Route::get('/hello', [TestController::class, 'hello']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => 'auth'], function() {
+    Route::group([
+        'middleware' => 'role:lecture',
+        'prefix' => 'lecture',
+        'as' => 'lecture.'
+    ], function(){
+        Route::get('/', [HomeController::class, 'lecture'])->name('show');
+    });
+
+    Route::group([
+        'middleware' => 'role:student',
+        'prefix' => 'student',
+        'as' => 'student.'
+    ], function(){
+        Route::get('/', [HomeController::class, 'student'])->name('show');
+    });
+});
